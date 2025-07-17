5a111149f97b6d720b58349ef43d3c2a524f4436
Nicolas Cribb <cribbnicolas26@gmail.com>
2025-07-17 15:28:17 -0300

    setted park ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import solidJs from '@astrojs/solid-js';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
@@ -9,11 +8,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [],
     resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-      '@tailwind': resolve(__dirname, './src/styles/global.css'),
       '@styles': resolve(__dirname, './src/styles')
     }
   },
